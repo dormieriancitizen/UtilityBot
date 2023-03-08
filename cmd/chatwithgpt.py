7cdec4d4ad441eb8ec7401d28b1ef5248d6fb978
UrMum <dormieriancitizen@gmail.com>
2023-03-07 22:49:38 -0500

    chat with gpt

--- cmd/chatwithgpt.py
+++ cmd/chatwithgpt.py
@@ -1,28 +1,20 @@
 import openai
-import os
-import asyncio
-import textwrap
+import asyncio, threading
 
-# Set up your OpenAI API key
-openai.api_key = "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"
+with open("./config/openaitoken.txt",'r') as token:
+    lines =  token.readlines()
+    openai.api_key = lines[0].rstrip('\n')
 
-# Define a function to send a prompt to ChatGPT and get a response
 async def chat(args,message,self):
-    if not self.ver:
-        return "BOT: i dont wanna run out of api tokens sry. but i might implement other ppls toeks later. "
-    # Set up the parameters for the completion request
-    
-    completions = openai.Completion.create(
-        engine="davinci",  # Use the Davinci engine for high-quality responses
-        prompt=" ".join(args)+"in english.",
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
+    reply = await message.reply("oki thinking")
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": args[0]},
+        ],
+        temperature=0,
     )
     
-    
-    for text in textwrap.wrap(completions.choices[0].text,1900):
-         await message.channel.send(text)
-    
+    await reply.edit(content=response['choices'][0]['message']['content'])
     return False
